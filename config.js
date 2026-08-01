b7b094eecdabf8e8f5578e28547946630f9eb2ed
archivebtw <archivebtw@gmail.com>
2026-08-02 02:43:39 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.AIM_CONFIG = {
   // Вставь данные проекта Supabase, чтобы включить общий онлайн-рейтинг.
   // Эти значения можно использовать на GitHub Pages. Никогда не вставляй service_role key.
-  supabaseUrl: "YOUR_SUPABASE_URL",
-  supabaseAnonKey: "YOUR_SUPABASE_ANON_KEY"
+  supabaseUrl: "https://edadwxggqqjugldngfll.supabase.co",
+  supabaseAnonKey: "sb_publishable_mOwditWAOlsz5Xq32b2g8g_6goNvfz0"
 };
